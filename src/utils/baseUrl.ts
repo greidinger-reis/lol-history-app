d5151a0f3d3a8032731319c8d771a45c02635d91
greidinger.dev <jp@greidinger.dev>
2023-02-20 14:04:00 -0300

    baseurl

--- src/utils/baseUrl.ts
+++
@@ -1,8 +0,0 @@
-export function getBaseUrl() {
-    if (typeof window !== "undefined")
-        return `${window.location.protocol}//${window.location.hostname}:${
-            process.env.PORT ?? 3000
-        }`; // browser should use relative url
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-}
