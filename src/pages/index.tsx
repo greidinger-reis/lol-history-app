8a9ae34a88185d53bd45749d4d187cfd7e112d19
greidinger.dev <jp@greidinger.dev>
2023-02-20 14:06:25 -0300

    fixed :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,10 @@ import Link from "next/link";
 import { useState } from "react";
 import { _Region } from "~/constants/regions";
 import { formatWhiteSpacesToUrl } from "~/utils/formatWhiteSpace";
-import { api } from "~/utils/api";
-import { Strategy } from "~/utils/tabnews";
 
 const Home: NextPage = () => {
     const [summonerName, setSummonerName] = useState("");
     const [region, setRegion] = useState("na");
-    const postsQuery = api.tabnews.listPosts.useInfiniteQuery(
-        { limit: 10, strategy: Strategy.NEW },
-        {
-            getNextPageParam: (prevPage) => prevPage.nextCursor,
-        }
-    );
 
     return (
         <>
@@ -47,26 +39,6 @@ const Home: NextPage = () => {
                 >
                     Search
                 </Link>
-                <ul>
-                    {postsQuery.data?.pages.map((page) =>
-                        page.posts.map((post) => (
-                            <li key={post.id}>
-                                <p>
-                                    {post.title} - {post.owner_username}
-                                </p>
-                            </li>
-                        ))
-                    )}
-                </ul>
-                <button
-                    className="rounded bg-blue-500 px-4 py-2 text-white transition-all duration-300 hover:bg-blue-600"
-                    /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
-                    onClick={() => postsQuery.fetchNextPage()}
-                >
-                    {postsQuery.isFetchingNextPage
-                        ? "Loading..."
-                        : "Load next page"}
-                </button>
             </main>
         </>
     );
